9830e1021f782c9c32edf29842c21bfaac0698df
AnjaliMusmade <anjalimusmade29@gmail.com>
2019-12-01 22:51:42 +0530

    1 file modified

--- components/HelloWorld.tsx
+++ components/HelloWorld.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 
 export default function HelloWorld(){
   return(
-  <div>
+  <div style={{backgroundColor:"green", padding:50}}>
   <div><h2>Hello World</h2></div>
   <button>Click ME</button>
   </div>
   )
 };
 
+function Handle(){
+  alert("First button clicked event fired!!!!!!!!!");
+}
 export function HelloWorld1(){
   return(
   <div>
   <div><h2>Hello World 1</h2></div>
-  <button>Click ME1</button>
+  <button onCLick={Handle}>Click ME1</button>
   </div>
   )
 };
